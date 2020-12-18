da17d4324f66cd41d3886d0aada1f88bbaac485d
elj40 <66779575+elj40@users.noreply.github.com>
2020-12-18 10:40:14 +0200

    Update DOM_elements.js

--- Self-driving car (ga)/DOM_elements.js
+++ Self-driving car (ga)/DOM_elements.js
@@ -13,6 +13,10 @@ var saveBest;
 var dropCarData;
 var bestOnly;
 var onlyBest = false;
+var joinIn;
+
+var pickColor;
+let c;
 
 function setupDOM() {
 	fr = select(".frameRate-holder");
@@ -31,15 +35,21 @@ function setupDOM() {
 	saveBest = select(".saveBest-button");
 	saveBest.mousePressed(saveBestCar);
 
+	joinIn = select(".joinIn-button");
+	joinIn.mousePressed(joinRace);
+
 	dropCarData = select(".drop-car-data");
 	dropCarData.dragOver(highlight);
 	dropCarData.dragLeave(unhighlight);
 	dropCarData.drop(gotFile, unhighlight);
 
+	pickColor = select(".myColor");
+
 	//Canvas styling
 	cnv.style('position' ,"relative");
 	cnv.style('left' ,"18%");
 	cnv.style("border-radius", "10px");
+
 }
 
 function dud() {
@@ -81,4 +91,22 @@ function newTrack(n) {
 function trackRestart(track_) {
 	track = track_;
 	restart();
+}
+
+function hexToRgb(hex) {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  } : null;
+}
+
+function joinRace() {
+	c = pickColor.value();
+	let col = hexToRgb(c);
+	let realCol = color(col.r, col.g, col.b);
+
+	if (player) player = null
+	else player = new Player(700, 100, realCol);
 }
